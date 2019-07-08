f523c12c1dafd4c84405c14e9efd218afa815310
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-08 23:05:15 +0800

    4.0 call appsettings service by subscribing in side detail component

--- src/app/product/product-detail.component.ts
+++ src/app/product/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppSettingsService } from '../Shared/app-settings.service';
+import { Product } from './product';
+import { AppSettings } from 'src/app/Shared/app-settings';
 
 @Component({
   selector: 'stt-product-detail',
@@ -6,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product :Product;
+  appSettings : AppSettings;
+  constructor(private appSettingsService  :AppSettingsService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+     this.appSettingsService.getSettings()
+     .subscribe(settings => this.appSettings =settings
+                ,() => null // error Handling section
+                ,() => {
+                  this.product = new Product();
+                  this.product.price = this.appSettings.defaultPrice;
+                  this.product.url = this.appSettings.defaultUrl;
+                }
+              );
   }
   saveProduct(){
 
