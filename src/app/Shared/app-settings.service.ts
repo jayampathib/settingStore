ffdd8d8b0ce2f045f8c1979c33273e9f5932cb67
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-09 23:01:35 +0800

    9.0 add error handling

--- src/app/Shared/app-settings.service.ts
+++ src/app/Shared/app-settings.service.ts
@@ -10,15 +10,38 @@ import { SETTING_LOCATION } from 'src/app/Shared/app-constant';
   providedIn: 'root'
 })
 export class AppSettingsService {
- 
+  
   constructor(private http:HttpClient) { 
-    
+
   }
+  private handleError  (operation ='operation', result?: AppSettings){
+    return(error :any) : Observable<AppSettings> =>{
+      switch (error.status){
+        case 404:
+          console.error("can't find Loacation :" + SETTING_LOCATION);
+          break;
+        default:
+          console.error(error);
+          break;
+      }
+
+      return of(result);
+    }
+
+  }
+ 
   getSettings():  Observable<AppSettings>{    
-    return this.http.get<AppSettings>(SETTING_LOCATION);
+    return this.http.get<AppSettings>(SETTING_LOCATION) 
+    .pipe(
+      catchError(this.handleError('getSettings', new AppSettings()))
+    );
+       
+     
   }
+
   // getSettings():  Observable<AppSettings>{
   //   let settings = new AppSettings();
   //   return of(settings);
   // }
+
 }
